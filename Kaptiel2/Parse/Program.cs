50960dffc72803a81cce883e1c9c161cde9510c9
torekelof <ekelof.tor@elev.ga.ntig.se>
2021-09-21 11:05:48 +0200

    Tränat på parse, int samt float

--- Kaptiel2/Parse/Program.cs
+++ Kaptiel2/Parse/Program.cs
@@ -6,7 +6,7 @@ namespace Parse
     {
         static void Main(string[] args)
         {
-            Console.Write("Hhur gammal är du? ");
+            Console.Write("Hur gammal är du? ");
             string ålder = Console.ReadLine();
             Console.WriteLine($"Du är {ålder} gammal");
             // Konvertera från string till heltal
@@ -16,6 +16,10 @@ namespace Parse
             //Använder en Float för vikt använd "," istället för "." i terminalen för att det är ett svenskt tangentbord
             float vikt = float.Parse(Console.ReadLine());
             Console.WriteLine($"Din vikt blir {vikt * 2.2} Lbs ");
+            float viktLbs = vikt * 2.2f;
+            //Formatera till en snygg utskrivning
+            string viktString = viktLbs.ToString("0.##");
+            Console.WriteLine($"Din vikt är {viktString} Lbs");
         }
     }
 }
---
+++ Kaptiel2/Uppgift-2.13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System;
+
+namespace Uppgift_2._13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mata in 3 löner");
+            //Ange 3 löner
+            int lön1 = int.Parse(Console.ReadLine());
+            int lön2 = int.Parse(Console.ReadLine());
+             int lön3 = int.Parse(Console.ReadLine());
+            //Medelvärdet
+            float Medelvärdet = (lön1 + lön2 + lön3) / 3;
+            //Resultatet
+            Console.WriteLine($"Medelvärdet är {Medelvärdet}");
+         }
+    }
+}
