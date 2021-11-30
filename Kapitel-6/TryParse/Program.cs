82f99d64fd28ef6bb3cb1812add520357da3dc93
Tor <90246344+Torekelof@users.noreply.github.com>
2021-11-30 10:26:45 +0100

    Tränat på tryparse

---
+++ Kapitel-6/TryParse/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace TryParse
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("gissa ett tal!");
+
+      //slumpgenerator
+
+      Random generator = new Random();
+
+      //slumpa fram ett tal mellan 1, 100
+
+      int slumtal = generator.Next(1, 101);
+      int slumtal2 = generator.Next(1, 11);
+
+
+    
+
+      int gissningTal = 0;
+      bool korrekt = false;
+
+
+      while (korrekt != true)
+      {
+        Console.WriteLine("Mata in ett tal (1.100)");
+        string gissning = Console.ReadLine();
+        korrekt = int.TryParse(gissning, out gissningTal);
+
+      }
+
+
+
+      //var gissningen korrekt
+      if (gissningTal == slumtal)
+      {
+        Console.WriteLine("Bra du gissade rätt");
+      }
+      else
+      {
+        Console.WriteLine("Tyvärr du gissade fel");
+      }
+
+    }
+  }
+}
---
+++ kapitel5/TP_frågor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TP_frågor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Slumpa fram en fråga");
+
+
+            string[] korten =
+            {"Vem vann fotbolls-EM 1984",                //0
+            "När uppfördes Verdi",                          //1
+            "Vem skrev Frankenstien",                          //2
+            "Vad är 5+5",                                         //3
+            "När startades andra världskriget",                      //4
+            "Vem är president i USA just nu"                            //5
+            };
+
+            Random tärning = new Random();
+            int slumptal = tärning.Next(0, 6);
+
+
+
+            Console.WriteLine($"Tärningen slog {slumptal}");
+
+            Console.WriteLine($"Frågan: {korten[slumptal]}");
+
+    }
+}
+}
